d935d2e7a88663116e3b23e8b58b258dfba4fd09
hdavidpb <68169674+hdavidpb@users.noreply.github.com>
2025-11-09 18:52:05 -0500

    [UPDATE] NotesApp.tsx

--- src/NotesApp.tsx
+++ src/NotesApp.tsx
@@ -34,7 +34,7 @@ function App() {
       <ThemeProvider>
         <RouterProvider router={appRouter} />
       </ThemeProvider>
-      {/* <ReactQueryDevtools /> */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
